af17b9d8b234e2f9f20fbda891e54a20a765cba6
roytam1 <roytam@gmail.com>
2022-10-14 16:41:38 +0800

    follow-up rev c1d7d28c, replace with https://stackoverflow.com/a/18255318/145278

--- gfx/angle/src/libGLESv2/mathutil.h
+++ gfx/angle/src/libGLESv2/mathutil.h
@@ -9,12 +9,22 @@
 #ifndef LIBGLESV2_MATHUTIL_H_
 #define LIBGLESV2_MATHUTIL_H_
 
-#if _MSC_VER <= 1400
-#define _interlockedbittestandreset _interlockedbittestandreset_NAME_CHANGED_TO_AVOID_MSVS2005_ERROR
-#define _interlockedbittestandset _interlockedbittestandset_NAME_CHANGED_TO_AVOID_MSVS2005_ERROR
+#if _MSC_VER >= 1400
+//  Following 8 lines: workaround for a bug in some older SDKs
+#   pragma push_macro("_interlockedbittestandset")
+#   pragma push_macro("_interlockedbittestandreset")
+#   pragma push_macro("_interlockedbittestandset64")
+#   pragma push_macro("_interlockedbittestandreset64")
+#   define _interlockedbittestandset _local_interlockedbittestandset
+#   define _interlockedbittestandreset _local_interlockedbittestandreset
+#   define _interlockedbittestandset64 _local_interlockedbittestandset64
+#   define _interlockedbittestandreset64 _local_interlockedbittestandreset64
+#   include <intrin.h> // to force the header not to be included elsewhere
+#   pragma pop_macro("_interlockedbittestandreset64")
+#   pragma pop_macro("_interlockedbittestandset64")
+#   pragma pop_macro("_interlockedbittestandreset")
+#   pragma pop_macro("_interlockedbittestandset")
 #endif
-
-#include <intrin.h>
 
 #include "common/system.h"
 #include "common/debug.h"
--- xpcom/glue/pldhash.cpp
+++ xpcom/glue/pldhash.cpp
@@ -6,13 +6,21 @@
 /*
  * Double hashing implementation.
  */
-#if _MSC_VER <= 1400
-#define _interlockedbittestandreset _interlockedbittestandreset_NAME_CHANGED_TO_AVOID_MSVS2005_ERROR
-#define _interlockedbittestandset _interlockedbittestandset_NAME_CHANGED_TO_AVOID_MSVS2005_ERROR
-#endif
-
 #if _MSC_VER >= 1400
-#include <intrin.h>
+//  Following 8 lines: workaround for a bug in some older SDKs
+#   pragma push_macro("_interlockedbittestandset")
+#   pragma push_macro("_interlockedbittestandreset")
+#   pragma push_macro("_interlockedbittestandset64")
+#   pragma push_macro("_interlockedbittestandreset64")
+#   define _interlockedbittestandset _local_interlockedbittestandset
+#   define _interlockedbittestandreset _local_interlockedbittestandreset
+#   define _interlockedbittestandset64 _local_interlockedbittestandset64
+#   define _interlockedbittestandreset64 _local_interlockedbittestandreset64
+#   include <intrin.h> // to force the header not to be included elsewhere
+#   pragma pop_macro("_interlockedbittestandreset64")
+#   pragma pop_macro("_interlockedbittestandset64")
+#   pragma pop_macro("_interlockedbittestandreset")
+#   pragma pop_macro("_interlockedbittestandset")
 #endif
 #include <stdio.h>
 #include <stdlib.h>
