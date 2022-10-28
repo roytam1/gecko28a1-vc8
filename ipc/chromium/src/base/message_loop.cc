182190c20c3c3c381e7fcdb7b5d4234510778b73
roytam1 <roytam@gmail.com>
2022-10-28 11:58:39 +0800

    backout "Bug 957828 - Reduce the number of copies involved in sending IPC Messages. r=bsmedberg, a=1.3+" and HACK in previous rev, so new tab page won't crash again.

--- ipc/chromium/src/base/message_loop.cc
+++ ipc/chromium/src/base/message_loop.cc
@@ -384,12 +384,7 @@ void MessageLoop::ReloadWorkQueue() {
     AutoLock lock(incoming_queue_lock_);
     if (incoming_queue_.empty())
       return;
-    //std::swap(incoming_queue_, work_queue_);
-    do {
-      PendingTask pending_task = incoming_queue_.front();
-      incoming_queue_.pop();
-      work_queue_.push(pending_task);
-    } while (!incoming_queue_.empty());
+    std::swap(incoming_queue_, work_queue_);
     DCHECK(incoming_queue_.empty());
   }
 }
