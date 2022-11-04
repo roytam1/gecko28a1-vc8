47d2792463271477e746860753cf384cc00800f9
roytam1 <roytam@gmail.com>
2022-11-04 11:19:45 +0800

    import from pm24_25-history: - Bug 919592+1019684 - Ionmonkey (ARM) sec fixes. (7c48d515) - Re-land Bug 995603 (59281f40) - HSTS preload list update (0fb906d7)

--- widget/cocoa/nsCocoaWindow.h
+++ widget/cocoa/nsCocoaWindow.h
@@ -323,9 +323,6 @@ public:
 
     void SetPopupWindowLevel();
 
-    bool IsChildInFailingLeftClickThrough(NSView *aChild);
-    bool ShouldFocusPlugin();
-
     NS_IMETHOD         ReparentNativeWidget(nsIWidget* aNewParent);
 protected:
 
