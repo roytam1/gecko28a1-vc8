9e736b11bda6a71edea91266cdb57d48340d1593
roytam1 <roytam@gmail.com>
2023-01-19 12:20:19 +0800

    import from pm24_25-history: - Initialize registers for cpuid to prevent misdetection of SIMD instruction sets. (5a311224) - Android: Bug 1055166 - Catch IllegalStateException when recycling Message (eb5a459b) - Mac: address build bustage in v25 (eba76703)

--- mobile/android/base/GeckoAppShell.java
+++ mobile/android/base/GeckoAppShell.java
@@ -2604,7 +2604,12 @@ public class GeckoAppShell
             Looper.myLooper().quit();
         else
             msg.getTarget().dispatchMessage(msg);
-        msg.recycle();
+
+        try {
+            msg.recycle();
+        } catch (IllegalStateException e) {
+            // There is nothing we can do here so just eat it
+        }
         return true;
     }
 
